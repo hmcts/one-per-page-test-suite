c03b585780ffa723752ee0ffd3cff39df169b112
Dominic Taylor <dom.taylor7@gmail.com>
2018-05-17 10:56:59 +0100

    add utils chai

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const content = require('./src/content');
+const chai = require('./utils/chai');
 
 module.exports.content = content;
+
+module.exports.expect = chai.expect;
+
+module.exports.sinon = chai.sinon;
