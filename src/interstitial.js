63b20cc545cca88209ff77334d3369f12f8d1b4d
Dominic Taylor <dom.taylor7@gmail.com>
2018-07-23 14:53:33 +0100

    allow to inject all application steps

--- src/interstitial.js
+++ src/interstitial.js
@@ -1,12 +1,13 @@
 const { testStep } = require('../utils/supertest');
 const httpStatus = require('http-status-codes');
 
-const navigatesToNext = (step, nextStep) => {
+const navigatesToNext = (step, nextStep, steps = []) => {
+  steps.push(nextStep)
   return testStep(step)
     .withSetup(req => {
       return req.session.generate();
     })
-    .withSteps(nextStep)
+    .withSteps(...steps)
     .post()
     .expect('Location', nextStep.path)
     .expect(httpStatus.MOVED_TEMPORARILY);
