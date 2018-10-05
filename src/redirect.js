0d853fb0b7b7a31cd57144bc315e0b7107e133b7
Dominic Taylor <dom.taylor7@gmail.com>
2018-10-05 14:58:36 +0100

    use session only when defined

--- src/redirect.js
+++ src/redirect.js
@@ -1,11 +1,13 @@
 const { testStep } = require('../utils/supertest');
 const httpStatus = require('http-status-codes');
 
-const navigatesToNext = (step, nextStep, session = {}) => {
-  return testStep(step)
-    .withSteps(nextStep)
-    .withSession(session)
-    .get()
+const navigatesToNext = (step, nextStep, session) => {
+  const test = testStep(step)
+    .withSteps(nextStep);
+  if (session) {
+    test.withSession(session);
+  }
+  return test.get()
     .expect('Location', nextStep.path)
     .expect(httpStatus.MOVED_TEMPORARILY);
 };
