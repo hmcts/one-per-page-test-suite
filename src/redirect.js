5df913366f2a7982ce905730db73733c115a1388
Dominic Taylor <dom.taylor7@gmail.com>
2018-10-05 14:16:00 +0100

    set session when redirect test

--- src/redirect.js
+++ src/redirect.js
@@ -1,9 +1,10 @@
 const { testStep } = require('../utils/supertest');
 const httpStatus = require('http-status-codes');
 
-const navigatesToNext = (step, nextStep) => {
+const navigatesToNext = (step, nextStep, session) => {
   return testStep(step)
     .withSteps(nextStep)
+    .withSession(session)
     .get()
     .expect('Location', nextStep.path)
     .expect(httpStatus.MOVED_TEMPORARILY);
