3605de643fc1820ab51ced0db959a457833f01cb
Mounika <mounika.ammineni@hmcts.net>
2018-10-10 13:36:05 +0100

    Spaces

--- src/interstitial.js
+++ src/interstitial.js
@@ -1,7 +1,7 @@
 const { testStep } = require('../utils/supertest');
 const httpStatus = require('http-status-codes');
 
-const navigatesToNext = (step, nextStep, steps = [], session={}) => {
+const navigatesToNext = (step, nextStep, steps = [], session = {}) => {
   steps.push(nextStep);
   return testStep(step)
     .withSetup(req => {
