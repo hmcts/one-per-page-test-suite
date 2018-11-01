ad50f30bec2cfa4a22f60bdf883e37a0dca4733e
J. Victor Ferreira <victorf@kainos.com>
2018-11-01 20:42:18 +0000

    Fresh test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # One per page test suite
 
 Used to assist with testing one per page steps
+
+ 
--- test/steps/Sample.step.js
+++ test/steps/Sample.step.js
@@ -1,6 +1,6 @@
-const { Question } = require('@hmcts/one-per-page');
+const { ExitPoint } = require('@hmcts/one-per-page');
 
-class SampleStep extends Question {
+class SampleStep extends ExitPoint {
   static get path() {
     return '/';
   }
